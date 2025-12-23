0990ab8c226e546a61433e1ace6486d7498c6fe0
Masseh Ziaie <mas154572@gmail.com>
2025-12-23 05:48:45 -0500

    finsihed structure of faq now working on add/minus icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { AccordionDemo } from "./AccordianDemo";
 function App() {
   return (
     <>
-      <main className="flex justify-center w-full h-screen bg-purple-100">
-        <div className="flex flex-col max-w-[600px] w-full mx-4">
-          <h1 className="text-6xl font-bold">FAQs</h1>
+      <main className="relative flex justify-center items-center w-full h-screen bg-purple-100">
+        <div className="absolute top-0 md:bg-[url('public/assets/images/background-pattern-desktop.svg')] bg-[url('public/assets/images/background-pattern-mobile.svg')] bg-cover h-[40%] w-full"></div>
+        <div className="flex flex-col max-w-[600px] w-full mx-4 bg-white p-4 rounded-[16px] relative z-10">
+          <h1 className="text-6xl font-bold text-purple-950">FAQs</h1>
           <AccordionDemo />
         </div>
       </main>
---
+++ src/components/ui/Buttons.tsx
@@ -0,0 +1,18 @@
+const IconPlus = () => {
+  return (
+    <img src="/assets/images/icon-plus.svg" alt="plus" width={24} height={24} />
+  );
+};
+
+const IconMinus = () => {
+  return (
+    <img
+      src="/assets/images/icon-minus.svg"
+      alt="minus"
+      width={24}
+      height={24}
+    />
+  );
+};
+
+export { IconPlus, IconMinus };
