eddb268af1173804b040008c3bb495ed6021fc22
Masseh Ziaie <mas154572@gmail.com>
2025-12-22 22:12:02 -0500

    ACTUALLY added accordian

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import "./App.css";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from "@radix-ui/react-accordion";
+import { AccordionDemo } from "./AccordianDemo";
 
 function App() {
   return (
     <>
-      <p className="text-sm">Hello</p>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <p className="text-lg">Hello</p>
+      <AccordionDemo />
     </>
   );
 }
